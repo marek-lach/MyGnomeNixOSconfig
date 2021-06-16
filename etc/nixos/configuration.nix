90abe429ca37bb82d36f3d6eee762a57711539df
Marek Ľach <graweeld@googlemail.com>
2021-06-16 12:46:41 +0200

    Update configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -94,7 +94,7 @@ in
   # Enable the GNOME Desktop Environment:
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  environment.gnome.excludePackages = [ pkgs.gnome.cheese pkgs.gnome.gnome-music pkgs.gnome.gnome-terminal pkgs.gnome.gnome-calendar pkgs.gnome.totem pkgs.gnome.geary pkgs.gnome.tali pkgs.gnome.iagno pkgs.gnome.hitori pkgs.gnome.atomix pkgs.gnome-tour ];
+  environment.gnome.excludePackages = [ pkgs.gnome.cheese pkgs.gnome.gnome-music pkgs.gnome.gnome-terminal pkgs.gnome.gedit pkgs.gnome.gnome-calendar pkgs.gnome.totem pkgs.gnome.geary pkgs.gnome.tali pkgs.gnome.iagno pkgs.gnome.hitori pkgs.gnome.atomix pkgs.gnome-tour ];
 
   # Configure keymap in X11:
    services.xserver.layout = "us,gb,sk";
@@ -165,6 +165,7 @@ in
      pandoc # A universal document converter
      ghostscript
      gnome.gspell
+     xed-editor # A less basic, basic text editor 
      libreoffice-fresh
      koreader # An ebook, and PDF reader
 
