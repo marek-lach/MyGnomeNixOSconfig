d54b39d62bb7fddf4a17da304269fae3db6b7bcb
Marek Ľach <graweeld@googlemail.com>
2021-07-18 10:45:12 +0200

    Update configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -1,12 +1,10 @@
 # Edit this configuration file to define what should be installed on
-# your system. You can apply it with 'nixos-rebuild switch' (--upgrade)
-# Help is available in the configuration.nix(5) man page
+# your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
-
 let
-unstable =
+nixos-unstable =
     fetchTarball
       https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
 nur = 
@@ -18,26 +16,31 @@ nur =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-  
-   nixpkgs.config = {
-    packageOverrides = in_pkgs: {
-    linuxPackages = in_pkgs.linuxPackages_latest;
+
+    nixpkgs.config = {
+
+    packageOverrides = pkgs: {
+    linuxPackages = pkgs.linuxPackages_zen; # Use the latest kernel
     nur = import nur {
-    unstable = import unstable {
+    nixos-unstable = import nixos-unstable {
     config = config.nixpkgs.config;
    };
     };
    };
   };
-    
-   # Use the systemd-boot EFI boot loader.
-   boot.loader.systemd-boot.enable = true;
-   boot.loader.efi.canTouchEfiVariables = true;
-   boot.supportedFilesystems = [ "btrfs" "ext4" "nfs4" "fuse" "fat32" ];
+  
+# Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  systemd.services.systemd-udev-settle.enable = false;
+  systemd.services.NetworkManager-wait-online.enable = false;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "btrfs" "ext4" "nfs4" "fuse" ];
 
-   # Networking:
-    networking.hostName = "halcek"; # Define your hostname.
-    networking.networkmanager.enable = true; # Sets-up the wireless network
+  # Networking:
+   networking.hostName = "halcek"; # Define your hostname.
+   networking.networkmanager.enable = true; # Sets-up the wireless network
+
+   services.mullvad-vpn.enable = true;
    
    # Workaround for the no network after resume issue:
     powerManagement.resumeCommands = ''
@@ -47,20 +50,14 @@ nur =
    
    # Sets the time zone:
    time.timeZone = "Europe/Bratislava";
-
-
-   # Per-interface useDHCP will be mandatory in the future, so this generated config
-   # replicates the default behaviour.
-    networking.useDHCP = false;
-    networking.interfaces.wlp2s0.useDHCP = true;
     
     # Select internationalisation properties.
-   i18n.defaultLocale = "en_GB.UTF-8";
+    i18n.defaultLocale = "en_GB.UTF-8";
     console = {
     font = "Lat2-Terminus16";
      keyMap = "us";
    };
-
+   
   # Allow updating firmware:
   hardware.enableAllFirmware = true;
   hardware.enableRedistributableFirmware = true;
@@ -76,14 +73,11 @@ nur =
   # Enable the GNOME Desktop Environment:
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  environment.gnome.excludePackages = [ pkgs.gnome.cheese pkgs.gnome.gnome-music pkgs.gnome.gnome-terminal pkgs.gnome.gedit pkgs.epiphany pkgs.gnome.gnome-calendar pkgs.gnome.totem pkgs.gnome.tali pkgs.gnome.iagno pkgs.gnome.hitori pkgs.gnome.atomix pkgs.gnome-tour ];
+  environment.gnome.excludePackages = [ pkgs.gnome.cheese pkgs.gnome-photos pkgs.gnome.gnome-music pkgs.gnome.gnome-ter>
 
   # Configure keymap in X11:
    services.xserver.layout = "us,gb,sk";
    services.xserver.xkbOptions = "eurosign:e";
-
-  # Allow UNfree licenses:
-    nixpkgs.config.allowUnfree = true;
 
   # Automatic system updates:
   system.autoUpgrade.enable = true;
@@ -99,6 +93,9 @@ nur =
 
   # Enable CUPS to print documents:
    services.printing.enable = true;
+
+  # Allow "unfree" packages.
+   nixpkgs.config.allowUnfree = true;
    
    # Font settings:
    fonts.fontconfig.enable = true;
@@ -110,18 +107,18 @@ nur =
     enableGhostscriptFonts = true;
     };
 
-  # Enable sound:
+# Enable sound:
    sound.enable = true;
    hardware.pulseaudio.enable = true;
    hardware.pulseaudio.support32Bit = true;
    hardware.pulseaudio.package = pkgs.pulseaudioFull;
    hardware.pulseaudio.zeroconf.discovery.enable = true;
    
-   # Required for screen-lock-on-suspend functionality.
-    services.logind.extraConfig = ''
-    LidSwitchIgnoreInhibited=False
-    HandleLidSwitch=suspend
-    HoldoffTimeoutSec=10
+ # Required for screen-lock-on-suspend functionality.
+   services.logind.extraConfig = ''
+   LidSwitchIgnoreInhibited=False
+   HandleLidSwitch=suspend
+   HoldoffTimeoutSec=10
   '';
   
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -131,13 +128,12 @@ nur =
       home = "/home/halcek";
      extraGroups = [ "wheel" "audio" "video" "network" "networkmanager"]; # Enable ‘sudo’ for the use>
    };
-   
-   # List of the packages installed in system profile. To search, run:
-   # $ nix search wget
-  
-   environment.systemPackages = with pkgs; [
 
-  # Editors and writig:
+# List packages installed in system profile. To search, run:
+  # $ nix search wget
+    environment.systemPackages = with pkgs; [
+
+    # Editors and writig:
      emacs  # Do not forget to add an editor to edit configuration.nix! The Nano editor is also insta>
      vnote  # For larger research documents
      trilium-desktop # Hierarchically linked notes
@@ -155,8 +151,8 @@ nur =
      hunspellDicts.en-gb-large
      aspellDicts.sk
      hunspellDicts.sk-sk
-
-   # VPNs and Firewall:
+     
+     # VPNs and Firewall:
      openvpn
      mullvad-vpn
      pptp
@@ -184,20 +180,20 @@ nur =
      tootle # A client for the fediverse
      cawbird # For Twitter
      
-   # Media
+     # Media
      cozy # Audio-books
      gnome-podcasts
      pragha # A competent music player
-     vlc
+     vlc # Media-files player
      easytag # Editing ID3 tags for musical files
      python38Packages.python-vlc
      python39Packages.python-vlc
      celluloid # Front-end for MPV
      youtube-dl # An internet video downloader
      sublime-music # A subsonic client
-     reaper # An affordable DAW
-
-    # System
+     reaper # An affordable DAW editor
+     
+     # System
       git
       wget
       unzip
@@ -219,22 +215,21 @@ nur =
       libgnome-keyring
       qgnomeplatform
       gnomeExtensions.hide-top-bar
-      gnomeExtensions.material-shell
       gnomeExtensions.showtime-horizontal
       gnomeExtensions.new-mail-indicator
       cinnamon.xapps
    ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-   programs.mtr.enable = true;
-   programs.gnupg.agent = {
-     enable = true;
-     enableSSHSupport = true;
-   };
    
-  # List the services that you want enabled:
+   # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+    programs.gnupg.agent = {
+    enable = true; 
+    enableSSHSupport = true; 
+  }; 
 
+  # List the services that you want to enable:
+   
   # Enable the OpenSSH daemon:
    services.openssh.enable = true;
    
@@ -261,7 +256,7 @@ nur =
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-  
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -269,4 +264,5 @@ nur =
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "21.05"; # Did you read the comment?
+
 }
