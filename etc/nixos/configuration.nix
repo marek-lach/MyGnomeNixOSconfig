903b9ed552c448e7fe2fb8f953dbc0c167ad43ac
Marek Ľach <graweeld@googlemail.com>
2021-07-20 22:02:43 +0200

    Update configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -1,6 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
+# How to install unfree packages on user level: NIXPKGS_ALLOW_UNFREE=1 nix-env -iA nixos.
 
 { config, pkgs, ... }:
 let
