03f51e0c07f96c9912fe281ef562d7352acceb89
Marek Ľach <graweeld@googlemail.com>
2021-07-23 14:44:13 +0200

    Update configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -335,7 +335,7 @@ NUR =
     networking.firewall.allowedTCPPorts = [ 53 80 88 442 433 443 444 445 514 554 5060 5228 5353 5357 8384 8443 31416 4419999 64738 ];
     networking.firewall.allowedUDPPorts = [ 53 88 8443 ];
   # Enable the firewall service itself:
-  networking.firewall.enable = true;
+    networking.firewall.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
