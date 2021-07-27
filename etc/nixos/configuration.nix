82243cbc7c9dac57185163c6c9d0d0daf3cbf736
Marek Ľach <graweeld@googlemail.com>
2021-07-27 17:01:48 +0200

    Update configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -175,7 +175,25 @@ NUR =
     load-module module-dbus-protocol
     load-module module-switch-on-connect # Switch automatically when Bluetooth connects
   '';
-  
+
+  hardware.pulseaudio.daemon.config = {
+    default-sample-rate = "48000";
+    alternate-sample-rate = "44100";
+    default-sample-channels = "2";
+    default-channel-map = "front-left,front-right";
+    default-fragments = "2";
+    default-fragment-size-msec = "125";
+    enable-lfe-remixing = "no";
+    high-priority = "yes";
+    nice-level = "-11";
+    realtime-scheduling = "yes";
+    realtime-priority = "9";
+    rlimit-rtprio = "9";
+    resample-method = "soxr-vhq";
+    daemonize = "no";
+    default-sample-format = "float32le";
+};
+
  # Run periodic garbage collection of the Nix store cache:
  nix.gc = {
     automatic = true;
